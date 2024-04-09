0f6bf92f752cba9217e92e3420f727a633c28d9b
rusty <blaze121304@gmail.com>
2024-04-09 15:18:19 +0900

    [payment system] - first commit

---
+++ src/test/kotlin/com/rusty/paymentsystem/PaymentSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rusty.paymentsystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PaymentSystemApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
